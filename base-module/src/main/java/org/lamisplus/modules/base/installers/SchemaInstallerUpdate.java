2aa300b27c96b19481b4d5cbafd7a49e0cf5f071
gichangi <gichangijohn3@gmail.com>
2022-05-20 12:25:23 +0100

    SchemaInstallerUpdate
    SchemaInstallerUpdate class rename

--- base-module/src/main/java/org/lamisplus/modules/base/installers/SchemaInstallerUpdate.java
+++ base-module/src/main/java/org/lamisplus/modules/base/installers/SchemaInstallerUpdate.java
@@ -5,9 +5,9 @@ import com.foreach.across.core.installers.AcrossLiquibaseInstaller;
 import org.springframework.core.annotation.Order;
 
 @Order(2)
-@Installer(name = "schema-installer-insert", description = "Insert the required database tables data", version = 1)
-public class SchemaInstallerInsert extends AcrossLiquibaseInstaller {
-    public SchemaInstallerInsert() {
+@Installer(name = "schema-installer-update", description = "Update the required database tables data", version = 1)
+public class SchemaInstallerUpdate extends AcrossLiquibaseInstaller {
+    public SchemaInstallerUpdate() {
         super("classpath:installers/base/schema/insert-schema-1.0.xml");
     }
 }
