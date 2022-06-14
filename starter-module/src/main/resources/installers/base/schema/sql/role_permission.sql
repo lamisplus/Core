4a5b78900d362cdbaf57d4a2887b8b8e59745898
Emeka <ilozuechukwuemeka@yahoo.com>
2022-06-14 16:12:29 +0100

    code clean up

--- starter-module/src/main/resources/installers/base/schema/sql/role_permission.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO role_permission (role_id, permission_id) VALUES (1, 1);
-INSERT INTO role_permission (role_id, permission_id) VALUES (1, 2);
-INSERT INTO role_permission (role_id, permission_id) VALUES (1, 3);
-INSERT INTO role_permission (role_id, permission_id) VALUES (1, 4);
