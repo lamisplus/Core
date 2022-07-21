c8a4929d883ac02c8e3bd0040cb66b70abd755cf
Emeka <ilozuechukwuemeka@yahoo.com>
2022-07-21 23:29:34 +0100

    feat: facility assignment and switching

---
+++ base-module/src/main/java/org/lamisplus/modules/base/domain/mapper/ApplicationUserOrganisationUnitMapper.java
@@ -0,0 +1,19 @@
+package org.lamisplus.modules.base.domain.mapper;
+
+
+import org.lamisplus.modules.base.domain.dto.ApplicationUserOrganisationUnitDTO;
+import org.lamisplus.modules.base.domain.entities.ApplicationUserOrganisationUnit;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface ApplicationUserOrganisationUnitMapper {
+
+    ApplicationUserOrganisationUnit toApplicationUserOrganisationUnit(ApplicationUserOrganisationUnitDTO applicationUserOrganisationUnitDTO);
+
+    ApplicationUserOrganisationUnitDTO toApplicationUserOrganisationUnitDTO(ApplicationUserOrganisationUnit applicationUserOrganisationUnit);
+
+    List<ApplicationUserOrganisationUnit> toApplicationUserOrganisationUnitList(List<ApplicationUserOrganisationUnitDTO> applicationUserOrganisationUnitDTOS);
+
+}
