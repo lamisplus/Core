f7701d5c9f962c226bd8463d9ed39dbfd96cad43
peter.abiodun <asquarep@yahoo.com>
2024-03-18 13:21:07 +0100

    updated version number, added query.yml

--- starter-module/src/main/webapp/jsx/pages/LoginPage.js
+++ starter-module/src/main/webapp/jsx/pages/LoginPage.js
@@ -162,7 +162,7 @@ function Register(props) {
             <div className=" col-sm-4"></div>
             <div className=" ml-10 col-sm-4">
               <p>
-                Copyright © LAMISPlus 2.1.0.1 |{" "}
+                Copyright © LAMISPlus 2.1.0 |{" "}
                 <Link to="/policy">
                   <b>Terms & Policy</b>
                 </Link>
