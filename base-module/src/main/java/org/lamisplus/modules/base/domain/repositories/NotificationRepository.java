886d42d022b317df3fa665f5e632c43a08f2eeb5
peterabiodun <peter.abiodun@thepalladiumgroup.com>
2024-11-01 07:35:58 +0100

    fix: update to notification flag

--- base-module/src/main/java/org/lamisplus/modules/base/domain/repositories/NotificationRepository.java
+++ base-module/src/main/java/org/lamisplus/modules/base/domain/repositories/NotificationRepository.java
@@ -26,7 +26,6 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
     @Query(value = "SELECT id, indicator, period FROM base_application_notification_config WHERE indicator = ?1 AND period =?2 AND facility_id = ?3 AND archived = 0", nativeQuery = true)
     Optional<Notification> findIndicatorByFacilityIdAndIndicator(String indicator, String period, Long facilityId);
 
-
     List<Notification> getAllByfacilityIdAndArchived(Long facilityId, int archived);
 
 
@@ -36,7 +35,7 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
 
     @Query(value = "SELECT  p.uuid, surname, first_name AS firstName, hospital_number AS hospitalNumber, sex, \n" +
             "CAST(EXTRACT(YEAR FROM AGE(NOW(), date_of_birth)) AS INTEGER) AS age, pharm.regimen,\n" +
-            "pharm.lastVisit, pharm.refill_period AS refillPeriod, pharm.appointmentDate, COUNT(*) OVER () AS totalCount\n" +
+            "pharm.lastVisit, pharm.refill_period AS refillPeriod, pharm.appointmentDate, caseManagerPatient.caseManagerName, COUNT(*) OVER () AS totalCount\n" +
             "FROM \n" +
             "patient_person p\n" +
             "INNER JOIN (\n" +
@@ -45,20 +44,24 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
             "    hap.visit_date AS lastVisit,\n" +
             "    hap.next_appointment AS appointmentDate,\n" +
             "    hap.refill_period,\n" +
-            "\textra->'regimens'->0->>'name' AS regimen,\n" +
-            "\tROW_NUMBER() OVER (PARTITION BY person_uuid ORDER BY hap.visit_date DESC) AS rn\n" +
+            "extra->'regimens'->0->>'name' AS regimen,\n" +
+            "ROW_NUMBER() OVER (PARTITION BY person_uuid ORDER BY hap.visit_date DESC) AS rn\n" +
             "FROM\n" +
             "    hiv_art_pharmacy hap\n" +
             "WHERE\n" +
             "    hap.archived = 0 AND hap.next_appointment = CAST (NOW() AS DATE)\n" +
             ") pharm ON pharm.person_uuid = p.uuid \n" +
+            "LEFT JOIN (\n" +
+            "SELECT DISTINCT ON (person_uuid) person_uuid, case_manager_id, CONCAT(cm.first_name, ' ', cm.last_name) AS caseManagerName, cm.phone_number AS phoneNumber from case_manager_patients cmp\n" +
+            "LEFT JOIN case_manager cm ON cm.id = cmp.case_manager_id\n" +
+            ") caseManagerPatient ON caseManagerPatient.person_uuid = p.uuid\n" +
             "WHERE p.facility_id = ?1 AND pharm.appointmentDate = CAST(NOW() AS DATE)", nativeQuery = true)
     List<AppointmentProjectionDto> getTodaysAppointment(Long facilityId);
 
 
     @Query(value = "SELECT  p.uuid, surname, first_name AS firstName, hospital_number AS hospitalNumber, sex, \n" +
             "CAST(EXTRACT(YEAR FROM AGE(NOW(), date_of_birth)) AS INTEGER) AS age, pharm.regimen,\n" +
-            "pharm.lastVisit, pharm.refill_period AS refillPeriod, pharm.appointmentDate, COUNT(*) OVER () AS totalCount\n" +
+            "pharm.lastVisit, pharm.refill_period AS refillPeriod, pharm.appointmentDate, caseManagerPatient.caseManagerName, COUNT(*) OVER () AS totalCount\n" +
             "FROM \n" +
             "patient_person p\n" +
             "INNER JOIN (\n" +
@@ -67,14 +70,18 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
             "    hap.visit_date AS lastVisit,\n" +
             "    hap.next_appointment AS appointmentDate,\n" +
             "    hap.refill_period,\n" +
-            "\textra->'regimens'->0->>'name' AS regimen,\n" +
-            "\tROW_NUMBER() OVER (PARTITION BY person_uuid ORDER BY hap.visit_date DESC) AS rn\n" +
+            "extra->'regimens'->0->>'name' AS regimen,\n" +
+            "ROW_NUMBER() OVER (PARTITION BY person_uuid ORDER BY hap.visit_date DESC) AS rn\n" +
             "FROM\n" +
             "    hiv_art_pharmacy hap\n" +
             "WHERE\n" +
             "    hap.archived = 0 AND hap.next_appointment >= date_trunc('week', CURRENT_DATE) \n" +
-            "\tAND hap.next_appointment < date_trunc('week', CURRENT_DATE) + INTERVAL '7 days'\n" +
+            "AND hap.next_appointment < date_trunc('week', CURRENT_DATE) + INTERVAL '7 days'\n" +
             ") pharm ON pharm.person_uuid = p.uuid \n" +
+            "LEFT JOIN (\n" +
+            "SELECT DISTINCT ON (person_uuid) person_uuid, case_manager_id, CONCAT(cm.first_name, ' ', cm.last_name) AS caseManagerName, cm.phone_number AS phoneNumber from case_manager_patients cmp\n" +
+            "LEFT JOIN case_manager cm ON cm.id = cmp.case_manager_id\n" +
+            ") caseManagerPatient ON caseManagerPatient.person_uuid = p.uuid\n" +
             "WHERE pharm.appointmentDate >= date_trunc('week', CURRENT_DATE) \n" +
             "AND  pharm.appointmentDate < date_trunc('week', CURRENT_DATE) + INTERVAL '7 days'\n" +
             "AND  p.facility_id = ?1", nativeQuery = true)
@@ -82,7 +89,7 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
 
     @Query(value = "SELECT hospital_number AS hospitalNumber, surname, first_name AS firstName,  sex, \n" +
             "CAST(EXTRACT(YEAR FROM AGE(NOW(), date_of_birth)) AS INTEGER) AS age, pharm.regimen,\n" +
-            "pharm.lastVisit, pharm.refill_period AS refillPeriod, pharm.appointmentDate, COUNT(*) OVER () AS totalCount\n" +
+            "pharm.lastVisit, pharm.refill_period AS refillPeriod, pharm.appointmentDate, caseManagerPatient.caseManagerName, COUNT(*) OVER () AS totalCount\n" +
             "FROM \n" +
             "patient_person p\n" +
             "INNER JOIN (\n" +
@@ -91,18 +98,21 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
             "    hap.visit_date AS lastVisit,\n" +
             "    hap.next_appointment AS appointmentDate,\n" +
             "    hap.refill_period,\n" +
-            "\textra->'regimens'->0->>'name' AS regimen,\n" +
-            "\tROW_NUMBER() OVER (PARTITION BY person_uuid ORDER BY hap.visit_date DESC) AS rn\n" +
+            "extra->'regimens'->0->>'name' AS regimen,\n" +
+            "ROW_NUMBER() OVER (PARTITION BY person_uuid ORDER BY hap.visit_date DESC) AS rn\n" +
             "FROM\n" +
             "    hiv_art_pharmacy hap\n" +
             "WHERE\n" +
             "    hap.archived = 0 AND hap.next_appointment >= date_trunc('month', CURRENT_DATE) \n" +
             "    AND hap.next_appointment < date_trunc('month', CURRENT_DATE) + INTERVAL '1 month'\n" +
             ") pharm ON pharm.person_uuid = p.uuid \n" +
+            "LEFT JOIN (\n" +
+            "SELECT DISTINCT ON (person_uuid) person_uuid, case_manager_id, CONCAT(cm.first_name, ' ', cm.last_name) AS caseManagerName, cm.phone_number AS phoneNumber from case_manager_patients cmp\n" +
+            "LEFT JOIN case_manager cm ON cm.id = cmp.case_manager_id\n" +
+            ") caseManagerPatient ON caseManagerPatient.person_uuid = p.uuid\n" +
             "WHERE pharm.appointmentDate >= date_trunc('month', CURRENT_DATE) \n" +
             "AND  pharm.appointmentDate < date_trunc('month', CURRENT_DATE) + INTERVAL '1 month'\n" +
             "AND  p.facility_id = ?1", nativeQuery = true)
-//    List<AppointmentProjectionDto> getMonthlyAppointments (Long facilityId);
     List<AppointmentProjectionDto> getMonthlyAppointments(Long facilityId);
 
 }
