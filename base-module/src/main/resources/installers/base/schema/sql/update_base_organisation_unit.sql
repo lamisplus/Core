f694f43f6101f4a2cd924007e15c6a05bb551b3e
TyavBarnabas2 <barnabas.tyav@thepalladiumgroup.com>
2025-12-12 11:55:05 +0100

    fixes:increase the installer

--- base-module/src/main/resources/installers/base/schema/sql/update_base_organisation_unit.sql
+++ base-module/src/main/resources/installers/base/schema/sql/update_base_organisation_unit.sql
@@ -7,3 +7,9 @@ WHERE bou.id = boui.organisation_unit_id AND boui.code = ANY (ARRAY['xCya42gPOnU
 UPDATE public.base_organisation_unit bou SET name = 'Birnin Kebbi Community Center'
     FROM base_organisation_unit_identifier boui
 WHERE bou.id = boui.organisation_unit_id AND boui.code = 'th3IMCg3lQ1';
+
+
+
+UPDATE public.base_organisation_unit bou SET name = 'Jalingo Community Center'
+    FROM base_organisation_unit_identifier boui
+WHERE bou.id = boui.organisation_unit_id AND boui.code = 'cmkm8UQpvWk';
